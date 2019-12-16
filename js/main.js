83504b581fbcf382c6bdc830f85be8b76a685088
chernyak88 <i_chernyak@inbox.ru>
2019-12-17 01:09:14 +0300

    finish, add adaptive

--- js/main.js
+++ js/main.js
@@ -55,3 +55,58 @@ previous.onclick = function() {
     next.classList.remove('active');
   }
 };
+
+///////////////// Вычисление высоты блока slider
+
+window.onload = function() {
+  let block = document.querySelector('.slider');
+  let banner = document.querySelector('.slider__img');
+  block.style.height = banner.height - 10 + 'px';
+};
+
+///////////////// Изменение высоты слайдера при ресайзе
+
+window.addEventListener('resize', function heightOfSlider(){
+  let block = document.querySelector('.slider');
+  let banner = document.querySelector('.slider__img');
+  block.style.height = banner.height - 10 + 'px';
+})
+
+//yandex maps location
+
+ymaps.ready(init);
+
+var placemarks = [
+    {
+        latitude: 59.897289,
+        longitude: 30.522013,
+
+    }
+],
+    geoObjects= [];
+
+function init() {
+    var map = new ymaps.Map('map', {
+        center: [59.896167, 30.490350],
+        zoom: 14,
+        controls: ['zoomControl'],
+        behaviors: ['drag']
+    });
+
+    for (var i = 0; i < placemarks.length; i++) {
+            geoObjects[i] = new ymaps.Placemark([placemarks[i].latitude, placemarks[i].longitude], {
+                hintContent: '<p class="hint">г. Санкт-Петербург, Мурманское шоссе, 12-й километр, 5</p>'
+            },
+                
+            {
+                iconLayout: 'default#image',
+                iconImageHref: './images/icons/map-marker.png',
+                iconImageOffset: [-23, -57],
+                iconImageSize: [46, 57]
+            });
+            map.geoObjects.add(geoObjects[i]);
+    };
+}
+
+  
+
